f616f519b302bcfa67e4187889ab907de51fc984
Abubakir Kais <bakir_janabi@yahoo.com>
2022-01-13 00:50:56 +0400

    Adding create user endpoint logic

--- service/users/create_user.go
+++ service/users/create_user.go
@@ -1,8 +1,44 @@
 package users
 
-import "github.com/Bruary/crypto-wallet/models"
+import (
+	"time"
 
-func (*Users) CreateUser(req interface{}) models.BaseResponse {
+	"github.com/Bruary/crypto-wallet/db"
+	"github.com/Bruary/crypto-wallet/models"
+	UUID "github.com/google/uuid"
+)
 
-	return models.BaseResponse{}
+func (*Users) CreateUser(req models.CreateUserRequest) models.BaseResponse {
+
+	user := models.User{
+		UUID:      UUID.New().String(),
+		FirstName: req.FirstName,
+		LastName:  req.LastName,
+		FullName:  req.FirstName,
+		Email:     req.Email,
+		Age:       req.Age,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	}
+
+	err := db.AddNewEntryToUsers(user)
+	if err != nil {
+		return models.BaseResponse{
+			Success: false,
+			Msg:     err.Error(),
+			Error: models.Error{
+				Code: 0,
+				Msg:  "lol it failed.",
+			},
+		}
+	}
+
+	return models.BaseResponse{
+		Success: true,
+		Msg:     "Creation done",
+		Error: models.Error{
+			Code: 0,
+			Msg:  "No Err.",
+		},
+	}
 }
