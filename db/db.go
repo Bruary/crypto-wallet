26a9db098ed4fbd4d0f8004e9ae9c7017667bdc4
Abubakir Kais <bakir_janabi@yahoo.com>
2022-01-12 22:57:09 +0400

    Creating users table

--- db/db.go
+++ db/db.go
@@ -8,6 +8,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
+var Db *sql.DB
+
 func ConnectToDB() {
 
 	var user = os.Getenv("db_user")
@@ -20,19 +22,39 @@ func ConnectToDB() {
 	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
 
 	// open database
-	db, err := sql.Open("postgres", psqlconn)
+	Db, err := sql.Open("postgres", psqlconn)
 	if err != nil {
 		panic(err)
 	}
 
 	// close database
-	defer db.Close()
+	defer Db.Close()
 
 	// check db
-	err = db.Ping()
+	err = Db.Ping()
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("Connected!")
+
+	createUsersTableQuery := `CREATE TABLE IF NOT EXISTS Users(
+		ID int PRIMARY KEY,
+		First_Name TEXT NOT NULL,
+		Last_Name TEXT NOT NULL,
+		Full_Name TEXT NOT NULL,
+		Email TEXT NOT NULL UNIQUE,
+		Age INT NOT NULL,
+		Created_at TIMESTAMP,
+		Updated_at TIMESTAMP,
+		Deteled_at TIMESTAMP
+		)`
+
+	_, err = Db.Exec(createUsersTableQuery)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Users Table Created/Found.")
+
 }
