51129e93e7cb05c07a31aefa3d2f84233f5a440f
Abubakir Kais <bakir_janabi@yahoo.com>
2022-01-13 00:49:21 +0400

    Added getDB method

--- db/db.go
+++ db/db.go
@@ -39,7 +39,8 @@ func ConnectToDB() {
 	fmt.Println("Connected!")
 
 	createUsersTableQuery := `CREATE TABLE IF NOT EXISTS Users(
-		ID int PRIMARY KEY,
+		ID SERIAL PRIMARY KEY,
+		UUID UUID UNIQUE,
 		First_Name TEXT NOT NULL,
 		Last_Name TEXT NOT NULL,
 		Full_Name TEXT NOT NULL,
@@ -56,5 +57,24 @@ func ConnectToDB() {
 	}
 
 	fmt.Println("Users Table Created/Found.")
+}
 
+func getDB() *sql.DB {
+
+	var user = os.Getenv("db_user")
+	var password = os.Getenv("db_password")
+	var port = os.Getenv("db_port")
+	var host = os.Getenv("db_host")
+	var dbName = os.Getenv("db_name")
+
+	// connection string
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+
+	// open database
+	Db, err := sql.Open("postgres", psqlconn)
+	if err != nil {
+		panic(err)
+	}
+
+	return Db
 }
